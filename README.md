a6ff600b2ffca3c2700a1ac97f1d0b1950988fc0
John Spataro <jspataro@vt.edu>
2018-02-05 22:56:48 -0500

    add mock cache, finish proof-of-concept script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # PySLCacheDebugger
-A cache searcher/debugger for the Second Life viewer texture cache.
+A cache searcher/debugger for the Second Life viewer texture cache. This project is still in initial development with the hopes of soon providing a user interface to browse the cache (thumbnails).
+
+## Dependencies
+* Python 3.6
+* [Glymur](https://github.com/quintusdias/glymur) and [OpenJPEG](http://www.openjpeg.org/)
+* Scipy
+* Numpy
+
+## Notes
+
+2/5/2018
+* Need to think about architecture to do asynchronous thumbnail fetch.
+* Textures will have to be fully generated on the fly when called for export. Can't fit entire cache in memory!
--- sandbox/entries_read.py
+++ sandbox/entries_read.py
@@ -4,17 +4,26 @@
 SANDBOX FILE
 THIS FILE IS NOT SOURCE CODE
 
-Cache header reading is explored here. 
+Cache header reading is explored here for proof of concept. To be formalized
+into actual source.
 
 "texture.entries" contains a general 44-byte header and N 28-byte entry headers
 "texture.cache" contains N 600-byte entries
 '''
 
 import os
-import sys
+import shutil
 import struct
+import sys
 from array import array
+from io import StringIO
 from pprint import pprint
+
+import glymur
+import numpy as np
+from scipy.misc import imsave
+
+BASE_CACHE_PATH = '..\\tests\\mock_texturecache\\texturecache'
 
 
 def format_uuid_from_ints(u8_vals):
@@ -27,7 +36,7 @@ def format_uuid_from_ints(u8_vals):
 # --- Open the cache entries file
 
 entries_file = open(
-    '../tests/mock_texturecache/texturecache/texture.entries',
+    os.path.join(BASE_CACHE_PATH, 'texture.entries'),
     'rb'
 )
 
@@ -56,12 +65,11 @@ encoder_version = unpacked[2].decode('utf-8').replace('\x00', '')
 entry_count = unpacked[3]
 
 
-pprint({
-    'Version': version,
-    'Address Size': address_size,
-    'Encoder Version': encoder_version,
-    'Entry Count': entry_count
-})
+print('VERSION:', version)
+print('ADDRESS SIZE:', address_size)
+print('ENCODER VERSION:', encoder_version)
+print('ENTRY COUNT:', entry_count)
+
 
 # ENTRY STRUCT BYTE LAYOUT
 #
@@ -88,13 +96,17 @@ for i in range(entry_count):
     except struct.error:
         break
 
+
+print('ENTRIES:', len(entries))
+
 # if this doesn't throw, we're golden!
 assert len(entries) == entry_count
+
 
 # --- Read entries from cache file
 
 cache_file = open(
-    '../tests/mock_texturecache/texturecache/texture.cache',
+    os.path.join(BASE_CACHE_PATH, 'texture.cache'),
     'rb'
 )
 
@@ -102,9 +114,97 @@ chunks = []
 for i in range(entry_count):
     chunks.append(cache_file.read(600))
 
+print('CHUNKS:', len(chunks))
+
 assert len(chunks) == entry_count
 
+# --- Find the rest of the cache chunk
 
-# Close the cache entries file
+# The first 600 bytes of the .j2c is located in the texture.cache file
+# while the remainder is inside various .texture files.
+
+bad_path_count = 0
+good_path_count = 0
+body_chunks = []
+for entry in entries:
+
+    uuid = entry[0]
+    uuid_start = uuid[0]
+
+    path_to_texture = os.path.join(
+        BASE_CACHE_PATH,
+        uuid_start,
+        uuid + ".texture"
+    )
+
+    if not os.path.exists(path_to_texture):
+        body_chunks.append(None)
+        bad_path_count += 1
+    else:
+        with open(path_to_texture, 'rb') as texfile:
+            body_chunks.append(texfile.read())
+            good_path_count += 1
+
+print('TOTAL BODIES:', len(body_chunks))
+print('GOOD BODIES:', good_path_count)
+print('MISSING BODIES:', bad_path_count)
+
+# --- Combine chunks to form full image
+
+complete_chunks = []
+for start, body in zip(chunks, body_chunks):
+
+    if body is not None:
+        complete_chunks.append(start + body)
+    else:
+        complete_chunks.append(start)
+
+print('COMPLETE CHUNKS:', len(complete_chunks))
+
+# --- Check to see if the result of combining chunks matches the
+# --- image size in the entry headers.
+
+for entry, chunk in zip(entries, complete_chunks):
+
+    # why is there a 600-byte deficit written
+    # into the expected size? looks like
+    # it's really the size of the body
+
+    expected_size = entry[1] + 600
+    actual_size = len(chunk)
+
+    if expected_size != actual_size:
+        print('MISMATCH:', expected_size, actual_size)
+
+    # also be sure the chunk starts with a J2C header (FF, 4F, FF, 51)
+    if chunk[0:4].hex() != 'ff4fff51':
+        print('BAD START:', chunk[0:4].hex())
+
+# --- Should have everything needed to recreate the image
+# --- using JPEG2000.
+
+if not os.path.exists('./output_j2c'):
+    os.makedirs('./output_j2c')
+
+if not os.path.exists('output_bmp'):
+    os.makedirs('./output_bmp')
+
+for entry, chunk in zip(entries, complete_chunks):
+
+    uuid = entry[0]
+    outp_path = os.path.join('output_j2c', uuid + '.j2c')
+
+    with open(outp_path, 'wb') as outfile:
+        outfile.write(chunk)
+
+    try:
+        img = glymur.Jp2k(outp_path)
+        inmem_img = img[:]
+        imsave(os.path.join('output_bmp', uuid + '.bmp'), inmem_img)
+    except glymur.lib.openjp2.OpenJPEGLibraryError:
+        print('Failed to export "%s"' % uuid)
+
+
+# --- Close open files
 entries_file.close()
 cache_file.close()
